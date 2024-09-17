05d52e6132b6c9324b8f3d8e4eb3c0c7d523123d
Viktor <viktor.taseski7@gmail.com>
2024-09-17 12:22:38 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const numbers = document.querySelectorAll(".number, .operations")
+const display = document.getElementsByClassName("divResult")[0]
+
+numbers.forEach(number => {
+    number.addEventListener("click", function (event) {
+        let input = event.target.innerHTML
+        if (input == 'x') {
+            input = "*"
+        }
+        appendToDisplay(input);
+    })
+})
+
+function appendToDisplay(input) {
+    if (display.value == '0') {
+        display.value = input;
+    } else {
+        display.value = display.value + input;
+    }
+
+    display.scrollLeft = display.scrollWidth;
+}
+function calculate() {
+    try {
+        display.value = parseFloat(eval(display.value).toFixed(5))
+        display.scrollLeft = 0;
+    } catch {
+        display.value = "Error"
+    }
+}
+
+function AC() {
+    display.value = ""
+}
